c2f8074241368f4fef7eff7106b2682c2d5270dc
mangy007 <manguatwork@gmail.com>
2023-07-03 00:58:10 +0530

    crawler link forwarding done

--- data-crawler/phishing_websites_crawler.py
+++ data-crawler/phishing_websites_crawler.py
@@ -1,24 +1,29 @@
 import scrapy
-import pandas as pd
+from scrapy import cmdline
+# import pandas as pd
 
 class PhishingWebsitesCrawler(scrapy.Spider):
+    '''
+        Phishtank phishing website crawler
+    '''
     name = 'phishing_websites_crawler'
     allowed_domains = ['phishtank.org']
     start_urls = ['https://phishtank.org/phish_search.php?verified=u&active=y']
+    limit = 1000
+    phishing_website_count = 0
 
     def parse(self, response):
-        websites_list = response.xpath('//div[@id="main"]//table[@class="data"]//tbody')
-        websites = []
+        # websites_list = response.xpath('//div[@id="main"]//div//div//table[@class="data"]//tbody')
+        websites_list = response.xpath('//*[@class="data"]//tr')
+        print("**********************************   aaya    ***************************************")
+        print("websites_list: ", len(websites_list))
 
-        flag = True
-        i = 0
-        for website in websites_list:
-            if i==10:
-                break
-            if flag:
+        for i, row in enumerate(websites_list[1].xpath('//td[1]//@href')):
+            if i==0:
                 continue
-            else:
-                print(website)
-            flag =  False
-            i += 1
+            # id = row.xpath('//td[1]//@href')
+            # print(i,"   ---->   ", row.xpath('//td[1]//text()'))
+            print(i, " --- ", row)
     
+    # def parse_phishing_webpage(self, response):
+    #     pass
