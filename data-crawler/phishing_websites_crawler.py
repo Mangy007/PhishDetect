5d705aaa52243f8e8d21eba537a11dd1773dbf52
mangy007 <manguatwork@gmail.com>
2023-07-03 14:47:43 +0530

    scrapy link extraction done

--- data-crawler/phishing_websites_crawler.py
+++ data-crawler/phishing_websites_crawler.py
@@ -1,6 +1,8 @@
 import scrapy
 from scrapy import cmdline
+import sys
 # import pandas as pd
+sys.stdout = open('output.txt','wt')
 
 class PhishingWebsitesCrawler(scrapy.Spider):
     '''
@@ -9,21 +11,23 @@ class PhishingWebsitesCrawler(scrapy.Spider):
     name = 'phishing_websites_crawler'
     allowed_domains = ['phishtank.org']
     start_urls = ['https://phishtank.org/phish_search.php?verified=u&active=y']
-    limit = 1000
+    limit = 1
     phishing_website_count = 0
 
     def parse(self, response):
         # websites_list = response.xpath('//div[@id="main"]//div//div//table[@class="data"]//tbody')
-        websites_list = response.xpath('//*[@class="data"]//tr')
+        websites_list = response.xpath('//*[@class="data"]')
         print("**********************************   aaya    ***************************************")
         print("websites_list: ", len(websites_list))
 
-        for i, row in enumerate(websites_list[1].xpath('//td[1]//@href')):
+        for i, row in enumerate(websites_list.xpath('tr')):
             if i==0:
                 continue
-            # id = row.xpath('//td[1]//@href')
-            # print(i,"   ---->   ", row.xpath('//td[1]//text()'))
-            print(i, " --- ", row)
+            print(i,"   ---->   ", row.xpath('td[1]//a//@href').extract_first())
+        
+        for i, row in enumerate(websites_list.xpath('td[5]')):
+            print(i,"   ---->   ", row.xpath('b//a//@href').extract_first())
+
     
     # def parse_phishing_webpage(self, response):
     #     pass
